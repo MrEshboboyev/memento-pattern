cd8a285c2d28790040ae517490623e06e68fe43f
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-06 17:06:26 +0500

    Memento Pattern in Console

---
+++ src/MementoPatternInConsole/Mementos/Concrets/ConcreteMemento.cs
@@ -0,0 +1,33 @@
+﻿namespace MementoPatternInConsole.Mementos.Concrets;
+
+// The Concrete memento contains the infrastructure for storing the Originator's state.
+internal class ConcreteMemento : IMemento
+{
+    private string state;
+
+    private DateTime _date;
+
+
+    public ConcreteMemento(string state)
+    {
+        this.state = state;
+        _date = DateTime.Now;
+    }
+
+    // The Originator uses this method when restoring its state.
+    public string GetState()
+    {
+        return state;
+    }
+
+    // The rest of the methods are used by the Caretaker to display metadata.
+    public string GetName()
+    {
+        return $"{_date} / ({state[..9]}...)";
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+}
---
+++ src/MementoPatternInConsole/Mementos/IMemento.cs
@@ -0,0 +1,10 @@
+﻿namespace MementoPatternInConsole.Mementos;
+
+// The Memento interface provides a way to retrieve the memento's metadata, such as
+// creation date or name. However, it doesn't expose the Originator's state.
+public interface IMemento
+{
+    string GetName();
+    string GetState();
+    DateTime GetDate();
+}
